1c5272d82692310258c133fe4109dac3b759272b
hanjala <shihab2305341402@diu.edu.bd>
2025-07-25 21:09:22 +0600

    added x label and y label

--- pyplot functions/practice4.py
+++ pyplot functions/practice4.py
@@ -4,6 +4,9 @@ months = [1,2,3,4]
 sales = [1000, 1500, 1200, 1800]
 
 plt.plot(months, sales, color = 'red', linestyle= '--', linewidth = 2, marker = 'o', label = '2025 sales data')
+plt.title("Monthly sales data report")
 
+plt.xlabel("Months")
+plt.ylabel("Sales per month")
 
 plt.show()
